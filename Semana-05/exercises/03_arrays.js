4fda0aa800be2d63d61ed1e9a67d87a2a7c7f93e
AnaMurinigo <anamurinigo@gmail.com>
2022-04-17 22:03:47 -0300

    BaSP2022: Exercise 3 with a strange way of copying the meses array

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -7,3 +7,36 @@ Invertir el orden del array (utilizar reverse).
 Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
 Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
 */
+//3-a
+var meses=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log("\n-Exercise 3.a:");
+console.log(meses[4], ", ", meses[10]);
+//3-b
+console.log("\n-Exercise 3.b:");
+var b=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(b.sort());
+console.log(meses);
+//3-c
+//I'm defining new variables because if i don't the results of the previous exercises are goin to be afected.
+var c= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+c.unshift("principio de año");
+c.push("fin de año");
+console.log("\n-Exercise 3.c:");
+console.log(c);
+//3-d
+var d=c;//Esto ahora perdió sentido
+d.shift();
+d.pop();
+console.log("\n-Exercise 3.d:");
+console.log(d);
+//3-e
+console.log("\n-Exercise 3.e:");
+console.log(d.reverse());
+//3-f
+console.log("\n-Exercise 3.f:");
+console.log(d.join("-"));
+//3-g
+console.log(meses);
+var g= meses.slice(4,-1);
+console.log("\n-Exercise 3.g:");
+console.log(g);
