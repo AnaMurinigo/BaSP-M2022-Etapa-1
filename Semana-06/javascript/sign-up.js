d77f7404329ded8eee8365ab436a2ff08231ee88
AnaMurinigo <anamurinigo@gmail.com>
2022-04-26 22:14:46 -0300

    BaSP2022: ordering the code

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,53 +1,233 @@
-// Getting the values for the elements
 var form = document.querySelector("form");
+//First Name
 var firstName = document.getElementById("name");
-var lastName = document.getElementById("last-name");
-var dni = document.getElementById("dni");
-var city = document.getElementById("city");
-var birthdate = document.getElementById("birthdate");
-var tel = document.getElementById("tel");
-var address = document.getElementById("address");
-var email = document.getElementById("email");
-var zip = document.getElementById("zip");
-var pwd = document.getElementById("pwd");
-var pwd2 = document.getElementById("pwd2");
-var emailError = document.querySelector(".email-error");
-var pwdError = document.querySelector(".pwd-error");
 var firstNameError = document.querySelector(".name-error");
+var firstNameValidator=false;
+firstName.onblur=firstNameBlur;
+firstName.onfocus=firstNameFocus;
+function firstNameBlur() {
+  firstNameValidator=false;
+  if (!notNull(firstName)) {
+    firstNameError.textContent = "The field cannot be empty";
+  } else if (!hasMinLength(firstName, 3)) {
+    firstNameError.textContent = "The name has too short";
+  } else if (!isText(firstName)) {
+    firstNameError.textContent = "The name cannot have numbers or symbols";
+  } else {
+    validateFirstName = true;
+  }
+}
+function firstNameFocus() {
+  firstNameError.textContent = "";
+}
+//LastName
+var lastName = document.getElementById("last-name");
 var lastNameError = document.querySelector(".last-name-error");
+var lastNameValidator=false;
+lastName.onblur=lastNameBlur;
+lastName.onfocus=lastNameFocus;
+function lastNameBlur() {
+  lastNameValidator=false;
+  if (!notNull(lastName)) {
+    lastNameError.textContent = "The field cannot be empty";
+  } else if (!hasMinLength(lastName, 3)) {
+    lastNameError.textContent = "The name has too short";
+  } else if (!isText(lastName)) {
+    lastNameError.textContent = "The name cannot have numbers or symbols";
+  } else {
+    lastNameValidator = true;
+  }
+}
+function lastNameFocus() {
+  lastNameError.textContent = "";
+}
+//DNI
+var dni = document.getElementById("dni");
 var dniError = document.querySelector(".dni-error");
-var cityError = document.querySelector(".city-error");
+var dniValidator=false;
+dni.onblur=dniBlur;
+dni.onfocus=dniFocus;
+function dniBlur() {
+  dniValidator=false;
+  if (!notNull(dni)) {
+    dniError.textContent = "The field cannot be empty";
+  } else if (!hasMinLength(dni, 7)) {
+    dniError.textContent = "The dni is too short";
+  } else if (isNaN(dni.value)) {
+    dniError.textContent = "The DNI only contains numbers";
+  } else {
+    dniValidator = true;
+  }
+}
+function dniFocus() {
+  dniError.textContent = "";
+}
+//BIRTHDATE
+var birthdate = document.getElementById("birthdate");
 var birthdateError = document.querySelector(".birthdate-error");
+var birthdateValidator=false;
+birthdate.onblur=birthdateBlur;
+birthdate.onfocus=birthdateFocus;
+function birthdateBlur(){
+  birthdateValidator=false;
+    if(!isLegal(birthdate)){
+        birthdateError.textContent="You must be older than 18"
+    }
+    else{
+      birthdateValidator=true;
+    }
+}
+function birthdateFocus() {
+  birthdateError.textContent = "";
+}
+//TEL
+var tel = document.getElementById("tel");
 var telError = document.querySelector(".tel-error");
+var telValidator=false;
+tel.onblur=telBlur;
+tel.onfocus=telFocus;
+function telBlur() {
+  telValidator=false;
+  if (tel.value.length != 10) {
+    telError.textContent = "The number has to have 10 digits";
+  } else if (isNaN(tel.value)) {
+    telError.textContent = "Should only contain numbers";
+  } else {
+    telValidator = true;
+  }
+}
+function telFocus() {
+  telError.textContent = "";
+}
+//ADDRESS
+var address = document.getElementById("address");
 var addressError = document.querySelector(".address-error");
+var addressValidator=false;
+address.onblur=addressBlur;
+address.onfocus=addressFocus;
+function addressBlur(){
+  addressValidator=false;
+  if(!hasMinLength(address, 5)){
+      addressError.textContent="The address is too short";
+  } else if(!hasMiddleSpace(address)){
+      addressError.textContent="It has to have a middle space";
+  } else{
+    addressValidator=true;
+  }
+}
+function addressFocus() {
+addressError.textContent = "";
+}
+//CITY
+var city = document.getElementById("city");
+var cityError = document.querySelector(".city-error");
+var cityValidator=false;
+city.onblur=cityBlur;
+city.onfocus=cityFocus;
+function cityBlur(){
+  cityValidator=false;
+  if(!hasMinLength(city,3)){
+      cityError.textContent="The city must have at least 3 letters"
+  } else{
+    cityValidator=true;
+  }
+}
+function cityFocus() {
+cityError.textContent = "";
+}
+//ZIP
+var zip = document.getElementById("zip");
 var zipError = document.querySelector(".zip-error");
+var zipValidator=false;
+zip.onblur=zipBlur;
+zip.onfocus=zipFocus;
+function zipBlur() {
+  zipValidator=false;
+  if (isNaN(zip.value)) {
+    zipError.textContent = "The zip must be a number";
+  } else if (!hasMinLength(zip, 4)) {
+    zipError.textContent = "The zip contains at least 4 or 5 numbers";
+  } else if (!maxLength(zip, 5)) {
+    zipError.textContent = "Contains 5 numbers max";
+  } else {
+    zipValidator = true;
+  }
+}
+function zipFocus() {
+  zipError.textContent = "";
+}
+//EMAIL
+var email = document.getElementById("email");
+var emailError = document.querySelector(".email-error");
+var emailValidator=false;
+email.onblur=emailBlur; //Está siempre escuchando
+email.onfocus=emailFocus;
+function emailBlur() {
+  emailValidator=false;
+  if (email.value === "") {
+    emailError.textContent = "This field cannot be null";
+  } else if (!email.value.match(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)) {
+    emailError.textContent = "Please enter a valid email";
+  }else{
+    emailValidator=true;
+  }
+}
+function emailFocus() {
+  emailError.textContent = "";
+}
+//PWD
+var pwd = document.getElementById("pwd");
+var pwdError = document.querySelector(".pwd-error");
+var pwdValidator=false;
+pwd.onblur=pwdBlur;
+pwd.onfocus=pwdFocus;
+function pwdBlur() {
+  if (pwd.value.length < 8) {
+    pwdValidator=false;
+    pwdError.textContent = "Your password is too short.";
+  } else if (!pwdValidate(pwd)) {
+    pwdError.textContent = "Your password must have numbers and letters.";
+  }else{
+    pwdValidator=true;
+  }
+}
+function pwdValidate() {
+  var hasLetters = false;
+  var hasNumbers = false;
+  for (var i = 0; i < pwd.value.length; i++) {
+    if (
+      pwd.value.charAt(i).toUpperCase() != pwd.value.charAt(i).toLowerCase()
+    ) {
+      hasLetters = true;
+    } else if (!isNaN(pwd.value.charAt(i))) {
+      hasNumbers = true;
+    }
+  }
+  return hasLetters && hasNumbers;
+}
+function pwdFocus() {
+  pwdError.textContent = "";
+}
+//REPEAT PWD
+var pwd2 = document.getElementById("pwd2");
 var pwd2Error = document.querySelector(".pwd2-error");
-//Listener
-firstName.addEventListener("blur", firstNameBlur);
-firstName.addEventListener("focus", firstNameFocus);
-lastName.addEventListener("blur", lastNameBlur);
-lastName.addEventListener("focus", lastNameFocus);
-dni.addEventListener("blur", dniBlur);
-dni.addEventListener("focus", dniFocus);
-city.addEventListener("blur", cityBlur);
-city.addEventListener("focus", cityFocus);
-birthdate.addEventListener("blur", birthdateBlur);
-birthdate.addEventListener("focus", birthdateFocus);
-tel.addEventListener("blur", telBlur);
-tel.addEventListener("focus", telFocus);
-address.addEventListener("blur", addressBlur);
-address.addEventListener("focus", addressFocus);
-//email.onblur(emailBlur);
-email.addEventListener("blur", emailBlur); //Está siempre escuchando
-email.addEventListener("focus", emailFocus);
-zip.addEventListener("blur", zipBlur);
-zip.addEventListener("focus", zipFocus);
-pwd.addEventListener("blur", pwdBlur);
-pwd.addEventListener("focus", pwdFocus);
-pwd2.addEventListener("blur", pwd2Blur);
-pwd2.addEventListener("focus", pwd2Focus);
-
-//Gral validations
+var pwd2Validator=false;
+pwd2.onblur=pwd2Blur;
+pwd2.onfocus=pwd2Focus;
+function pwd2Blur() {
+  pwd2Validator=false;
+  if ((pwd.value == "")) {
+    pwd2Error.textContent = "Please first fill in the password field";
+  } else if (pwd2.value !== pwd.value) {
+    pwd2Error.textContent = "Your passwords are different";
+  } else{
+    pwd2Validator=true;
+  }
+}
+function pwd2Focus() {
+  pwd2Error.textContent = "";
+}
+//Gral functions validatiors
 function notNull(something) {
   if (something.value === "") {
     return false;
@@ -92,7 +272,6 @@ function hasMiddleSpace(something) {
   }
   return false;
 }
-
 function hasMinLength(something, minValue) {
   if (something.value.length < minValue) {
     return false;
@@ -110,218 +289,3 @@ function isLegal(something){
     var dateNow=new Date(Date.now());
     return new Date(dateNow-inputDate).getFullYear()-1970>=18;
 }
-//Email validation
-function emailBlur() {
-  if (email.value === "") {
-    emailError.textContent = "This field cannot be null";
-    // email.insertAdjacentElement("afterend",elem);
-  } else if (!email.value.match(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)) {
-    emailError.textContent = "Please enter a valid email";
-    // firstName.parentNode.insertAdjacentHTML("afterend","Please enter a valid email");
-  }
-}
-function emailFocus() {
-  emailError.textContent = "";
-}
-//first name
-function firstNameBlur() {
-  if (!notNull(firstName)) {
-    firstNameError.textContent = "The field cannot be empty";
-  } else if (!hasMinLength(firstName, 3)) {
-    firstNameError.textContent = "The name has too short";
-  } else if (!isText(firstName)) {
-    firstNameError.textContent = "The name cannot have numbers or symbols";
-  } else {
-    var validateFirstName = true;
-  }
-}
-function firstNameFocus() {
-  firstNameError.textContent = "";
-}
-//LastName
-function lastNameBlur() {
-  if (!notNull(lastName)) {
-    lastNameError.textContent = "The field cannot be empty";
-  } else if (!hasMinLength(lastName, 3)) {
-    lastNameError.textContent = "The name has too short";
-  } else if (!isText(lastName)) {
-    lastNameError.textContent = "The name cannot have numbers or symbols";
-  } else {
-    var validateLastName = true;
-  }
-}
-function lastNameFocus() {
-  lastNameError.textContent = "";
-}
-//DNI
-function dniBlur() {
-  if (!notNull(dni)) {
-    dniError.textContent = "The field cannot be empty";
-  } else if (!hasMinLength(dni, 7)) {
-    dniError.textContent = "The dni is too short";
-  } else if (isNaN(dni.value)) {
-    dniError.textContent = "The DNI only contains numbers";
-  } else {
-    var validateDniName = true;
-  }
-}
-function dniFocus() {
-  dniError.textContent = "";
-}
-//CITY
-function cityBlur(){
-    if(!hasMinLength(city,3)){
-        cityError.textContent="The city must have at least 3 letters"
-    } else{
-        var cityValidate=true;
-    }
-}
-function cityFocus() {
-  cityError.textContent = "";
-}
-//BIRTH DATE
-function birthdateBlur(){
-    if(!isLegal(birthdate)){
-        birthdateError.textContent="You must be older than 18"
-    }
-    else{
-        var validateBirthday=true;
-    }
-}
-function birthdateFocus() {
-  birthdateError.textContent = "";
-}
-//TEL
-function telBlur() {
-  if (tel.value.length != 10) {
-    telError.textContent = "The number has to have 10 digits";
-  } else if (isNaN(tel.value)) {
-    telError.textContent = "Should only contain numbers";
-  } else {
-    var validateTel = true;
-  }
-}
-function telFocus() {
-  telError.textContent = "";
-}
-//ADDRESS
-function addressBlur(){
-    if(!hasMinLength(address, 5)){
-        addressError.textContent="The address is too short";
-    } else if(!hasMiddleSpace(address)){
-        addressError.textContent="It has to have a middle space";
-    } else{
-        var validateAddress=true;
-    }
-}
-function addressFocus() {
-  addressError.textContent = "";
-}
-//ZIP
-function zipBlur() {
-  if (isNaN(zip.value)) {
-    zipError.textContent = "The zip must be a number";
-  } else if (!hasMinLength(zip, 4)) {
-    zipError.textContent = "The zip contains at least 4 or 5 numbers";
-  } else if (!maxLength(zip, 5)) {
-    zipError.textContent = "Contains 5 numbers max";
-  } else {
-    var validateZip = true;
-  }
-}
-function zipFocus() {
-  zipError.textContent = "";
-}
-//pwd
-function pwdBlur() {
-  if (pwd.value.length < 8) {
-    pwdError.textContent = "Your password is too short.";
-  } else if (!pwdValidate(pwd)) {
-    pwdError.textContent = "Your password must have numbers and letters.";
-  }
-}
-function pwdValidate() {
-  var hasLetters = false;
-  var hasNumbers = false;
-  for (var i = 0; i < pwd.value.length; i++) {
-    if (
-      pwd.value.charAt(i).toUpperCase() != pwd.value.charAt(i).toLowerCase()
-    ) {
-      hasLetters = true;
-    } else if (!isNaN(pwd.value.charAt(i))) {
-      hasNumbers = true;
-    }
-  }
-  return hasLetters && hasNumbers;
-}
-function pwdFocus() {
-  pwdError.textContent = "";
-}
-//Repeat pwd
-function pwd2Blur() {
-  if ((pwd.value == "")) {
-    pwd2Error.textContent = "Please first fill in the password field";
-  } else if (pwd2.value !== pwd.value) {
-    pwd2Error.textContent = "Your passwords are different";
-  }
-}
-function pwd2Focus() {
-  pwd2Error.textContent = "";
-}
-//Name
-//     function firstNameBlur() {
-//         if(firstName.value<4){
-//             emailParagraph.textContent="The name is too short";
-//         }
-//     }
-//        function firstNameFocus(){
-//            emailParagraph.textContent="";
-//        }
-//     //LastName
-//     function lastNameBlur() {
-//         if(firstName.value<4){
-//             emailParagraph.textContent="The name is too short";
-//         }
-//     }
-//     function lastNameFocus(){
-//            emailParagraph.textContent="";
-//        }
-// var aasd=document.createElement("p");
-// emailParagraph.insertAdjacentText("afterbegin", "edwqeqwewqewq");
-// emailPar
-
-// function validate(field) {
-//     // Get the  value of the input field being submitted
-//     value = document.getElementById(field).value;
-//     // Set the error field tag in the html
-//     errorField = field + 'Error';
-//     // Set the success field
-//     successField = field + 'Success';
-//     if (value != '') {
-//         document.getElementById(successField).style.display = 'block';
-//         document.getElementById(errorField).style.display = 'none';
-//         validated[field] = true;
-//     } else {
-//         document.getElementById(successField).style.display = 'none';
-//         document.getElementById(errorField).style.display = 'block';
-//         validated[field] = false;
-//     }
-// }
-// function SimulateSubmit() {
-//     // Query your elements
-//     var inputs = document.getElementsByTagName('input');
-
-//     // Loop your elements
-//     for (i = 0, len = inputs.length; i < len; i++) {
-
-//         // Ignore inputs without a validate function call
-//         if (!inputs[i].getAttribute('onblur').match(/^validate/)) {
-//             continue
-//         }
-//         var name = inputs[i].id;
-//         if (!validated[name]) {
-//             // Call validate
-//             validate(name);
-//             // Prevent default
-//         }
-//     }
